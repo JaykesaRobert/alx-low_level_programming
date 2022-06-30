4e7f066cdf0684eae2cf95829514eee72b547d0a
JaykesaRobert <kesa1996@outlook.com>
2022-06-30 13:33:42 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
   *
   * Return: Always 0 (Success)
   */
-  int main(void)
-  {
-          puts("\"Programming is like building a multilingual puzzle");
-          return (0);
-  } 
+ int main(void)
+ {
+         puts("\"Programming is like building a multilingual puzzle");
+         return (0);
+ } 
