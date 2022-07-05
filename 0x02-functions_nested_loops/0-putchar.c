6c5562e2d355398aa11a32c888279d75fae89614
JaykesaRobert <kesa1996@outlook.com>
2022-07-05 10:21:24 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	const char str[] = "_putchar\n";
+	int len = strlen(str);
+	int i;
+
+	for (i = 0; i < len; i++)
+		_putchar(str[i]);
+	return (0);
+}
